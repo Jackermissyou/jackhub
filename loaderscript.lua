c2b3250944d82ec5bc2a3000e7f5249ac3e3ffd6
Jackermissyou <145672309+Jackermissyou@users.noreply.github.com>
2024-01-17 14:59:25 +0700

    Update loaderscript.lua

--- loaderscript.lua
+++ loaderscript.lua
@@ -1,1 +1,8 @@
 print("anh nhớ em quá")
+local Notification = require(game:GetService("ReplicatedStorage").Notification)
+Notification.new("<Color=Yellow> Loading <Jack Hub> <Color=/>"):Display()
+Notification.new("<Color=Red> Made by Jacker<Color=/>"):Display()
+Notification.new("<Color=Blue> Discord:https://discord.gg/RE3aBJHdMa <Color=/>"):Display()
+local PlaceId = game.PlaceId
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/Jackermissyou/jackhub/main/jackhub.lua")()
