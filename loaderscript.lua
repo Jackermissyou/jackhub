403b368420423d913f4622325a74755d947022b5
Jackermissyou <145672309+Jackermissyou@users.noreply.github.com>
2024-01-25 12:57:40 +0700

    Update loaderscript.lua

--- loaderscript.lua
+++ loaderscript.lua
@@ -13,4 +13,4 @@ print(Platform)
 
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-https://raw.githubusercontent.com/Jackermissyou/jackhub/main/jackhub.lua
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Jackermissyou/jackhub/main/jackhub.lua")()
