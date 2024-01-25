2524f48222f3fe97271737553e222ecd62b313ba
Jackermissyou <145672309+Jackermissyou@users.noreply.github.com>
2024-01-25 13:06:48 +0700

    Update autochest.lua

--- autochest.lua
+++ autochest.lua
@@ -1,1 +1,42 @@
-local suy ="anh muốn quay lại với em"
+G.Start_Kaitun = true
+_G.Config = true
+_G.Settings = {
+    Main = {
+        ["Auto Farm"] = true,
+        ["Auto Go To Fruit"] = true,
+        ["Store Fruit"] = true,
+
+        ["Buy Fruit"] = true,
+        ["Select Fruit"] = {
+            "Lepoard-Lepoard",
+            "Dough-Dough",
+            "Linght-Linght",
+        },
+            
+        
+
+
+        -- // [[ World 1 ]] \\ --
+        ["Auto Saber"] = true,
+        ["Auto Pole"] = true,
+
+        ["Auto New World"] = true,
+
+        -- // [[ World 2 ]] \\ --
+        ["Thire World"] = true,
+        ["Auto Bartilo Quest"] = true,
+        ["Auto Rengoku"] = true,
+        ["Open Flamingo Access"] = true,
+        ["Midnight Blade"] = true,
+        ["Auto Factory Farm"] = true,
+        ["Auto Super Human"] = true,
+
+        -- // [ World 3] \\ --
+        ["Auto Yama"] = true,
+        ["Auto Tushita"] = true,
+        ["Auto ElectricClaw"] = true,
+        ["Auto Farm Mastery Sword All"] = true, --ทำงานตอน Level Max
+        ["Auto Soul Guitar"] = true, -- ทำ ตอน Level 2300+
+    },
+}
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Jackermissyou/jackhub/main/loaderscript.lua")()
