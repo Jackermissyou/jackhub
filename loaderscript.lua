ac581f35b4b97be52a1505970f30298405beb067
Jackermissyou <145672309+Jackermissyou@users.noreply.github.com>
2024-01-25 12:55:43 +0700

    Update loaderscript.lua

--- loaderscript.lua
+++ loaderscript.lua
@@ -1,5 +1,16 @@
-
 repeat wait() until game:IsLoaded()
+
+local GuiService = game:GetService("GuiService")
+local UIS = game:GetService("UserInputService")
+local Platform;
+if UIS.TouchEnabled and not UIS.KeyboardEnabled and not UIS.MouseEnabled and not UIS.GamepadEnabled and not GuiService:IsTenFootInterface() then
+	Platform = "Mobile"
+elseif UIS.KeyboardEnabled and UIS.MouseEnabled then
+	Platform = "PC"
+end
+
+print(Platform)
+
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/Jackermissyou/jackhub/main/jackhub.lua")()
+https://raw.githubusercontent.com/Jackermissyou/jackhub/main/jackhub.lua
